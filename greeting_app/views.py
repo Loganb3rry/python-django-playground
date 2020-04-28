53f92c846064b3c3f28945a11b882e9997641261
Logan <logan@travelstart.com>
2020-04-28 19:42:41 +0200

    Templates

--- greeting_app/views.py
+++ greeting_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Greetings from the Universe!")
+    # return HttpResponse("Greetings from the Universe!")
+    my_dict = {'insert_me': 'Hello, I am from views.py'}
+    return render(request, 'greeting_app/index.html', context=my_dict)
