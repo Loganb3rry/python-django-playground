45ab2cf8020446de1ffb6820ecb3dbd1c345b690
Logan <logan@travelstart.com>
2020-05-01 20:50:15 +0200

    CRUD Views

--- school_app/urls.py
+++ school_app/urls.py
@@ -7,5 +7,7 @@ app_name = 'school_app'
 urlpatterns = [
     path('', views.SchoolListView.as_view(), name="list"),
     path('<int:pk>/', views.SchoolDetailView.as_view(), name="detail"),
-    # re_path(r'^(?P<pk>[-\w]+)/$', views.SchoolDetailView.as_view(), name="detail"),
+    path('create/', views.SchoolCreateView.as_view(), name="create"),
+    path('update/<int:pk>/', views.SchoolUpdateView.as_view(), name="update"),
+    path('delete/<int:pk>/', views.SchoolDeleteView.as_view(), name="delete"),
 ]
--- school_app/views.py
+++ school_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (View, TemplateView,
+                                  ListView, DetailView,
+                                  CreateView, UpdateView, DeleteView)
 from school_app import models
 
 
@@ -16,3 +18,18 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'school_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('school_app:list')
